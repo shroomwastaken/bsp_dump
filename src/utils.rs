b86b665b793d459403abac38fd39b6fd1bb20855
shroomwastaken <xeoplay123456@gmail.com>
2024-03-16 23:26:15 +0300

    impl Display for Vector3

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,14 @@
+use core::fmt;
+
 #[derive(Debug, Clone, Copy)]
 pub struct Vector3 {
 	pub x: f32,
 	pub y: f32,
 	pub z: f32,
+}
+
+impl fmt::Display for Vector3 {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "({}, {}, {})", self.x, self.y, self.z)
+	}
 }
