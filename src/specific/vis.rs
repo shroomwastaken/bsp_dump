86f97b216f204deab428e1b6d839274611991308
shroomwastaken <xeoplay123456@gmail.com>
2024-04-07 23:04:00 +0300

    remove overrun print and fix bug

--- src/specific/vis.rs
+++ src/specific/vis.rs
@@ -1,5 +1,5 @@
 // https://github.com/ValveSoftware/source-sdk-2013/blob/master/sp/src/utils/common/bsplib.cpp#L1417
-// https://www.flipcode.com/archives/Quake_2_BSP_File_Format.shtml
+// https://www.flipcode.com/archives/Quake_2_BSP_File_Format.shtml <-- i use this
 
 // returns a vec of bools where true means the cluster is visible
 // and false means it isnt
@@ -7,7 +7,7 @@ pub fn decompress_vis(inp: &[u8], num_clusters: &i32) -> Vec<bool> {
 	let mut res: Vec<bool> = vec![false; *num_clusters as usize];
 	let mut c: usize = 0;
 	let mut v: usize = 0;
-	while c < *num_clusters as usize{
+	while c < *num_clusters as usize {
 		if inp[v] == 0 {
 			v += 1;
 			c += 8 * inp[v] as usize;
@@ -16,15 +16,16 @@ pub fn decompress_vis(inp: &[u8], num_clusters: &i32) -> Vec<bool> {
 			let mut bit: u8 = 1;
 			while bit != 0 {
 				res[c] = (inp[v] & bit) != 0;
-				bit <<= 2; c += 1;
+				bit <<= 1; c += 1;
 				if c >= *num_clusters as usize {
-					println!("warning overrun");
+					// this is an overrun, dunno why it happens
+					// my code is probably wrong somewhere idk
+					// i'll just break for now and figure it out later
 					break;
 				}
 			}
 		}
 		v += 1;
 	}
-
 	res
 }
